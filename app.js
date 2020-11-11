6df9ab25d96a6e1b429c9591fff929d94d8c3830
mellow <mellowjscollapal@gmail.com>
2020-11-11 17:35:03 +0000

    made grid

---
+++ app.js
@@ -0,0 +1,18 @@
+// use two for loops to make a grid
+
+const container = document.querySelector('#container');
+
+function makeRows(rows, cols) {
+    container.style.setProperty('--grid-rows', rows);
+    container.style.setProperty('--grid-cols', cols);
+    for (c = 0; c < (rows * cols); c++) {
+        let cell = document.createElement("div");
+        cell.innerText = ("");
+        container.appendChild(cell).className = "grid-item";
+    };
+
+};
+
+
+
+makeRows(16, 16);
