88a32a41567c36842550738d49456e99332b848e
mellow <mellowjscollapal@gmail.com>
2020-11-12 23:15:00 +0000

    made grid, added hover for painting effect, and added button to clear grid

--- app.js
+++ app.js
@@ -1,18 +1,41 @@
-// use two for loops to make a grid
-
 const container = document.querySelector('#container');
+const clearGridBtn = document.querySelector('#clear-grid-btn')
 
-function makeRows(rows, cols) {
+function makeGrid(rows, cols) {
+
+    let cell;
     container.style.setProperty('--grid-rows', rows);
     container.style.setProperty('--grid-cols', cols);
-    for (c = 0; c < (rows * cols); c++) {
-        let cell = document.createElement("div");
+    for (let c = 0; c < (rows * cols); c++) {
+        cell = document.createElement("div");
         cell.innerText = ("");
         container.appendChild(cell).className = "grid-item";
     };
 
+    const containerItems = container.querySelectorAll('div');
+    
+    for (let i = 0; i < containerItems.length; i++) {
+        containerItems[i].addEventListener('mouseenter', function() {
+            containerItems[i].classList.add('grid-item-hover');
+        });
+    }
+
+    clearGridBtn.addEventListener('click', function() {
+        for (item of containerItems) {
+            item.classList.remove('grid-item-hover');
+        }
+    });
+
+
+
 };
 
+popUp() {
+    
+}
 
 
-makeRows(16, 16);
+
+makeGrid (16, 16);
+
+
